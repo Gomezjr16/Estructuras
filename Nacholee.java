3375923299a9c01a540c9797a33ea021529f8271
Sala K 405 <salak405@salasfr.itm.edu.co>
2026-02-09 09:15:29 -0500

    suma.proyecto

--- Nacholee.java
+++ Nacholee.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 public class Nacholee {
     
     
     public static void main(String [] ars){
     System.out.println("hola mundo");
-    } 
+    
+    
+
+    int n1=0, n2=0, suma=0;
+    scanner sc = new scanner (system.in);
+    System.out.println("ingrese el numero 1");
+    n1= sc.nextline;
+    System.out.println("ingrese el numero 2");
+    n2= sc.nextline;
+    System.out.println("la suma de los dos numeros es"+n1+n2);
+
+} 
 
 
 
